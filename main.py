d975a1217e74168d9d60f40adb3529eacd5e3e75
Daniel Corcoran <danielc.92@outlook.com>
2019-04-02 15:04:15 +1100

    Added navbar template

--- main.py
+++ main.py
@@ -2,31 +2,32 @@ from flask import Flask, redirect, url_for, session, render_template
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/todoDB"
+app.config['SECRET_KEY'] = 'top-secret'
+app.config['MONGO_URI'] = "mongodb://localhost:27017/todoDB"
 mongo = PyMongo(app)
 
 # Main routes
 @app.route('/register')
 def register():
-	return 'register'
+    return 'register'
 
 @app.route('/login')
 def login():
-	return 'login'
+    return 'login'
 
-@app.route('/tasks')
-def tasks():
-	return 'tasks'
+@app.route('/')
+def home():
+    return render_template('base.html', title='Todo - Home')
 
 @app.route('/members')
 def members():
-	return 'members'
+    return 'members'
 
 # Test routes
 @app.route('/query-users')
 def qusers():
-	return 'users query'
+    return 'users query'
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
